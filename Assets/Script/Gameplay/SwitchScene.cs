ab821d7ba7729c9a5fc962c90f36431d07a3ad1e
loctodale <thang336655@gmail.com>
2024-06-27 12:13:44 +0700

    add pallet town house

--- Assets/Script/Gameplay/SwitchScene.cs
+++ Assets/Script/Gameplay/SwitchScene.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,22 +9,57 @@ public class SwitchScene : MonoBehaviour
 {
     public string nextScene;
     public BoxCollider2D gate;
+    private PlayerController player;
 
-    // Start is called before the first frame update
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] DestinationIdentifier destinationPortal;
     void Start()
     {
-
+        GameObject playerObject = GameObject.Find("Player");
+        if (playerObject != null && playerObject.TryGetComponent(out PlayerController controller))
+        {
+            this.player = controller;
+        }
+        else
+        {
+            Debug.LogError("Player or PlayerController not found.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "Player")
-            SceneManager.LoadSceneAsync(nextScene);
+        {
+            player = other.gameObject.GetComponent<PlayerController>(); 
+            if (player != null)
+            {
+
+                StartCoroutine(SwitchScenes());
+            }
+        }
     }
+
+    IEnumerator SwitchScenes()
+    {
+        Transform parentTransform = transform.root;
+        DontDestroyOnLoad(parentTransform.gameObject);
+        yield return SceneManager.LoadSceneAsync(nextScene);
+        var destPortal = FindObjectsOfType<SwitchScene>().FirstOrDefault(x => x != this && x.destinationPortal == destinationPortal);
+        if (destPortal != null)
+        {
+            player.transform.position = destPortal.spawnPoint.position;
+        }
+        else
+        {
+            Debug.LogError("Destination portal not found.");
+        }
+        Destroy(parentTransform.gameObject);
+    }
+   
+
+
+    public Transform SpawnPoint => spawnPoint;
 }
+
+public enum DestinationIdentifier { A, B, C, D, E }
